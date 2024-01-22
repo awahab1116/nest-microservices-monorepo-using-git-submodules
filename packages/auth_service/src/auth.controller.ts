d0308abe816bb8fc8f5eed440599caf81e1e25b0
awahab1116 <awahab1116@gmail.com>
2024-01-22 16:38:07 +0500

    added events from microservice submodule

--- packages/auth_service/src/auth.controller.ts
+++ packages/auth_service/src/auth.controller.ts
@@ -3,16 +3,17 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
 import { User } from './entity/user.entity';
 import { RegisterUserDto, LoginUserDto } from './auth.dto';
+import { LOGIN_USER_EVENT, REGISTER_USER_EVENT } from '@microservice/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @MessagePattern({ cmd: 'register_user' })
+  @MessagePattern({ cmd: REGISTER_USER_EVENT })
   async registerUser(@Payload() message: RegisterUserDto): Promise<User> {
     return await this.authService.registerUser(message);
   }
 
-  @MessagePattern({ cmd: 'login_user' })
+  @MessagePattern({ cmd: LOGIN_USER_EVENT })
   async loginUser(@Payload() message: LoginUserDto): Promise<any> {
     return await this.authService.loginUser(message);
   }
